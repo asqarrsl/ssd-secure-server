414ae52d9627ef425a0c9c6cda425f9b130a607b
Mohamed Ashique Ar Rasool <it19243740@my.sliit.lk>
2022-11-17 16:47:19 +0530

    Server

--- routes/file.route.js
+++ routes/file.route.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const { uploadFile } = require('../controllers/file.controller');
 const upload = require("../middleware/multer.middleware")
 
+
+const multer = require('multer');
+const {storage} = require('../cloudinary')
+const upload = multer({storage})
+
 router.post('/upload', upload.single('file'), uploadFile);
 
 module.exports = router;
