042957abbc30d05d648858dcc3bce09cf5cf3be7
Mohamed Ashique Ar Rasool <it19243740@my.sliit.lk>
2022-11-17 12:10:17 +0530

    first commit

--- index.js
+++ index.js
@@ -11,38 +11,28 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 // app.use(cors({ origin: ['http://localhost:3000', 'https://abc-company-client.vercel.app'], credentials: true }));
-// app.use(cors({ origin: ['http://localhost:3000', 'https://abc-company-client.vercel.app'], credentials: true }));
-// app.use('/api/public/uploads', express.static(path.join(__dirname, '/api/public/uploads')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// const sslServer = https.createServer(
-//   {
-//     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-//     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-//   },
-//   app
-// );
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+  },
+  app
+);
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
-  // .then(() => {
-  //   // sslServer.listen(PORT, () => console.log(`Secure server listening on port ${PORT}`));
-  // })
-  // .catch((err) => {
-  //   console.error(err.message);
-  // });
-  const db = mongoose.connection;
-  db.on("error",console.error.bind(console,"connection error"));
-  db.once("open",()=>{
-      console.log("Database Connected");
-  });
-
-  app.get('/', (req, res) => {
-    res.send('<h3>ABC COMPANY API</h3>');
+  .then(() => {
+    sslServer.listen(PORT, () => console.log(`Secure server listening on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error(err.message);
   });
   
 app.use('/message', require('./routes/message.route'));
@@ -50,4 +40,6 @@ app.use('/user', require('./routes/user.route'));
 app.use('/admin', require('./routes/admin.route'));
 app.use('/file', require('./routes/file.route'));
 
-
+app.get('/', (req, res) => {
+  res.send('<h3>ABC COMPANY API</h3>');
+});
