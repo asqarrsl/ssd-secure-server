60d9855a8cd0ba8718da3c4997543fd20d8de88a
Mohamed Ashique Ar Rasool <it19243740@my.sliit.lk>
2022-11-17 16:38:52 +0530

    Server

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 // app.use(cors({ origin: ['http://localhost:3000', 'https://abc-company-client.vercel.app'], credentials: true }));
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
